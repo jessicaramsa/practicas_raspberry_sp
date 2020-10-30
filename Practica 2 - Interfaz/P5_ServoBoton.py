06e905ead6939f91d9bd31f82a5114966c5bef27
Daniel Estrada <isc.daniel.estrada@gmail.com>
2020-10-30 15:34:48 -0600

    ...

--- Practica 2 - Interfaz/P5_ServoBoton.py
+++ Practica 2 - Interfaz/P5_ServoBoton.py
@@ -1,22 +1,43 @@
 ##Ejercicio 5 - Servo con boton
-from tkinter import *
-from gpiozero import AngularServo
+'''from tkinter import *
+from gpiozero import Servo
 
 def ServoMotor():
     global abierto
+    
     if abierto:
-        sermot.angle=0
-        servoEtqBoton="Abrir"
+        sermot.angle = 0
+        servoEtqBoton = "Abrir"
     else:
-        sermot.angle=90
-        servoEtqBoton="Cerrar"
+        sermot.angle = 90
+        servoEtqBoton = "Cerrar"
+        
     btn_servo.configure(text=servoEtqBoton)
-    abierto=not ruido
+    abierto = not ruido
 
-gpioservo=16
-abierto=False
-sermot=Buzzer(gpioservo)
-root=Tk()
-btn_servo=Button(root, text="Abrir",command=ServoMotor)
+gpioservo = 25
+abierto = False
+sermot = Servo(gpioservo)
+root = Tk()
+btn_servo=Button(root, text="Abrir", command=ServoMotor)
 btn_servo.pack(anchor=CENTER)
-root.mainloop()
+root.mainloop()'''
+
+from gpiozero import Servo
+from time import sleep
+
+servo = Servo(25)
+
+while True:
+    servo.mid()
+    print("mid")
+    sleep(0.5)
+    servo.min()
+    print("min")
+    sleep(1)
+    servo.mid()
+    print("mid")
+    sleep(0.5)
+    servo.max()
+    print("max")
+    sleep(1)
